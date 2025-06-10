6877202058fd9ac29bd1b2732399bec28a15b077
Craig Miller <cmiller@radiantlogic.com>
2025-06-10 16:31:12 -0400

    Fixing script

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 mvn clean compile \
   exec:exec@generate \
-  -DopenapiPath=https://developer.radiantlogic.com/apiSpecifications/radiantone-openapi-8.1.3.yaml
+  -DopenapiPath=$(pwd)/radiantone-openapi-8.1.4-beta.2-SNAPSHOT.yaml

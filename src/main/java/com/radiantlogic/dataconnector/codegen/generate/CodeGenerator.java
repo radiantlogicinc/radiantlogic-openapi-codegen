2f647c89b2e85d47929830c08d4bbd5f251dea6a
Craig Miller <cmiller@radiantlogic.com>
2025-06-11 11:01:59 -0400

    Fixing compile errors

--- src/main/java/com/radiantlogic/dataconnector/codegen/generate/CodeGenerator.java
+++ src/main/java/com/radiantlogic/dataconnector/codegen/generate/CodeGenerator.java
@@ -1,28 +1,23 @@
 package com.radiantlogic.dataconnector.codegen.generate;
 
-import io.swagger.parser.OpenAPIParser;
-import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.parser.core.models.ParseOptions;
-import java.util.List;
-import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import org.openapitools.codegen.ClientOptInput;
-import org.openapitools.codegen.DefaultGenerator;
 
 @RequiredArgsConstructor
 public class CodeGenerator {
-  @NonNull private final ParsedConfig config;
 
   public void generate() {
     final ParseOptions parseOptions = new ParseOptions();
     parseOptions.setResolve(true);
     parseOptions.setResolveFully(true);
 
-    final OpenAPI openAPI =
-        new OpenAPIParser()
-            .readLocation(config.openapiPath(), List.of(), parseOptions)
-            .getOpenAPI();
-    final DataconnectorJavaClientCodegen codegen = new DataconnectorJavaClientCodegen(openAPI);
-    new DefaultGenerator().opts(new ClientOptInput().config(codegen).openAPI(openAPI)).generate();
+    //    final OpenAPI openAPI =
+    //        new OpenAPIParser()
+    //            .readLocation(config.openapiPath(), List.of(), parseOptions)
+    //            .getOpenAPI();
+    //    final DataconnectorJavaClientCodegen codegen = new
+    // DataconnectorJavaClientCodegen(openAPI);
+    //    new DefaultGenerator().opts(new
+    // ClientOptInput().config(codegen).openAPI(openAPI)).generate();
   }
 }

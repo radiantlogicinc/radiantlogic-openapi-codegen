d282fa2ef79cab7594325fa919c942d27db6b89e
Craig Miller <cmiller@radiantlogic.com>
2025-06-11 13:04:11 -0400

    Progress

--- end-to-end-tests/iddm-api-test/src/test/java/com/radiantlogic/dataconnector/test/DatasourceTest.java
+++ end-to-end-tests/iddm-api-test/src/test/java/com/radiantlogic/dataconnector/test/DatasourceTest.java
@@ -4,23 +4,22 @@ import com.radiantlogic.custom.dataconnector.api.AuthTokenApiApi;
 import com.radiantlogic.custom.dataconnector.invoker.ApiClient;
 import org.junit.jupiter.api.Test;
 
-import java.nio.charset.StandardCharsets;
-import java.util.Base64;
-
 public class DatasourceTest {
-    @Test
-    void itCanCreateAndReadDatasource() {
-        final ApiClient apiClient = new ApiClient();
-        apiClient.setBasePath("https://rlqa-usw2-craig.dev01.radiantlogic.io");
+  @Test
+  void itCanCreateAndReadDatasource() {
+    final ApiClient apiClient = new ApiClient();
+    apiClient.setBasePath("https://rlqa-usw2-craig.dev01.radiantlogic.io");
 
-        final AuthTokenApiApi authTokenApiApi = new AuthTokenApiApi(apiClient);
-        final String username = System.getenv("IDDM_USERNAME");
-        final String password = System.getenv("IDDM_PASSWORD");
-        authTokenApiApi.postLogin(Base64.getEncoder().encodeToString("%s:%s".formatted(username, password).getBytes(StandardCharsets.UTF_8))).getToken();
-
-        apiClient.setBearerToken(() -> {
-            System.getenv()
-            return authTokenApiApi.postLogin().getToken();
-        });
-    }
+    final AuthTokenApiApi authTokenApiApi = new AuthTokenApiApi(apiClient);
+    final String username = System.getenv("IDDM_USERNAME");
+    final String password = System.getenv("IDDM_PASSWORD");
+    //
+    // authTokenApiApi.postLogin(Base64.getEncoder().encodeToString("%s:%s".formatted(username,
+    // password).getBytes(StandardCharsets.UTF_8))).getToken();
+    //
+    //        apiClient.setBearerToken(() -> {
+    //            System.getenv()
+    //            return authTokenApiApi.postLogin().getToken();
+    //        });
+  }
 }

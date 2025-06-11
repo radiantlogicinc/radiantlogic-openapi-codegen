4bc602b26286bca5e16435ae52fa98ad9e4201d0
Craig Miller <cmiller@radiantlogic.com>
2025-06-11 08:51:44 -0400

    Small refactor

--- src/main/java/com/radiantlogic/dataconnector/codegen/generate/CodeGenerator.java
+++ src/main/java/com/radiantlogic/dataconnector/codegen/generate/CodeGenerator.java
@@ -1,8 +1,8 @@
 package com.radiantlogic.dataconnector.codegen.generate;
 
 import io.swagger.parser.OpenAPIParser;
+import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.parser.core.models.ParseOptions;
-import io.swagger.v3.parser.core.models.SwaggerParseResult;
 import java.util.List;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
@@ -14,12 +14,11 @@ public class CodeGenerator {
   @NonNull private final ParsedConfig config;
 
   public void generate() {
-    final SwaggerParseResult parseResult =
-        new OpenAPIParser().readLocation(config.openapiPath(), List.of(), new ParseOptions());
-    final DataconnectorJavaClientCodegen codegen =
-        new DataconnectorJavaClientCodegen(parseResult.getOpenAPI());
-    new DefaultGenerator()
-        .opts(new ClientOptInput().config(codegen).openAPI(parseResult.getOpenAPI()))
-        .generate();
+    final OpenAPI openAPI =
+        new OpenAPIParser()
+            .readLocation(config.openapiPath(), List.of(), new ParseOptions())
+            .getOpenAPI();
+    final DataconnectorJavaClientCodegen codegen = new DataconnectorJavaClientCodegen(openAPI);
+    new DefaultGenerator().opts(new ClientOptInput().config(codegen).openAPI(openAPI)).generate();
   }
 }

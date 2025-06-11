f270221064dccdb8ae664dae471cae21fba675bf
Craig Miller <cmiller@radiantlogic.com>
2025-06-11 12:30:19 -0400

    Implementing pre-processing

--- src/main/java/com/radiantlogic/dataconnector/codegen/generate/CodeGenerator.java
+++ src/main/java/com/radiantlogic/dataconnector/codegen/generate/CodeGenerator.java
@@ -2,9 +2,12 @@ package com.radiantlogic.dataconnector.codegen.generate;
 
 import com.radiantlogic.dataconnector.codegen.args.Args;
 import io.swagger.parser.OpenAPIParser;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.parser.core.models.ParseOptions;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,10 +26,24 @@ public class CodeGenerator {
     parseOptions.setResolve(true);
     parseOptions.setResolveFully(true);
 
+    log.debug("Parsing OpenAPI specification");
     final OpenAPI openAPI =
         new OpenAPIParser().readLocation(args.openapiPath(), List.of(), parseOptions).getOpenAPI();
+
+    preProcessOpenAPI(openAPI);
+
+    log.debug("Performing code generation");
     final DataconnectorJavaClientCodegen codegen =
         new DataconnectorJavaClientCodegen(openAPI, args);
     new DefaultGenerator().opts(new ClientOptInput().config(codegen).openAPI(openAPI)).generate();
+  }
+
+  private void preProcessOpenAPI(@NonNull final OpenAPI openAPI) {
+    log.debug("Pre-processing OpenAPI");
+    Optional.ofNullable(openAPI.getComponents())
+        .map(Components::getSchemas)
+        .orElseGet(Map::of)
+        // Clear the title to prevent accidental identification of duplicate schemas
+        .forEach((key, schema) -> schema.setTitle(null));
   }
 }

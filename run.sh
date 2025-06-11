3fafa7148dfd296fe1e6908d2610d66244a85c4b
Craig Miller <cmiller@radiantlogic.com>
2025-06-11 09:51:34 -0400

    Working on tests

--- run.sh
+++ run.sh
@@ -4,4 +4,5 @@ set -euo pipefail
 
 mvn clean compile \
   exec:exec@generate \
-  -DprogramArgs="-p=$(pwd)/radiantone-openapi-8.1.4-beta.2-SNAPSHOT.yaml"
+  -DprogramArgs='-h'
+#  -DprogramArgs="-p=$(pwd)/radiantone-openapi-8.1.4-beta.2-SNAPSHOT.yaml"
---
+++ src/test/java/com/radiantlogic/dataconnector/codegen/args/ArgsParserTest.java
@@ -0,0 +1,20 @@
+package com.radiantlogic.dataconnector.codegen.args;
+
+import org.junit.jupiter.api.Test;
+
+public class ArgsParserTest {
+  @Test
+  void itShowsHelpMenu() {
+    throw new RuntimeException();
+  }
+
+  @Test
+  void itParsesWithDefaults() {
+    throw new RuntimeException();
+  }
+
+  @Test
+  void itParsesWithAllArgsProvided() {
+    throw new RuntimeException();
+  }
+}

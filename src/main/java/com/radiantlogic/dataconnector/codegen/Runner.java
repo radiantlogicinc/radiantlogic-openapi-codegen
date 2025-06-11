a5c88345f4161be432dd420fd8ffad77b80e2467
Craig Miller <cmiller@radiantlogic.com>
2025-06-11 09:16:36 -0400

    Added more logging

--- src/main/java/com/radiantlogic/dataconnector/codegen/Runner.java
+++ src/main/java/com/radiantlogic/dataconnector/codegen/Runner.java
@@ -3,7 +3,9 @@ package com.radiantlogic.dataconnector.codegen;
 import com.radiantlogic.dataconnector.codegen.generate.CodeGenerator;
 import com.radiantlogic.dataconnector.codegen.generate.ConfigParser;
 import com.radiantlogic.dataconnector.codegen.generate.ParsedConfig;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class Runner {
   private static final String COMMAND_GENERATE = "generate";
 
@@ -11,6 +13,8 @@ public class Runner {
     if (args.length == 0) {
       throw new IllegalArgumentException("Cannot execute with specifying the command");
     }
+
+    log.info("Starting program");
 
     if (COMMAND_GENERATE.equals(args[0])) {
       final ParsedConfig config = new ConfigParser().parseArgs(args);

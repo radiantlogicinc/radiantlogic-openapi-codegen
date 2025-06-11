9f4010ac4486b0e116e576d78de03739d5d54854
Craig Miller <cmiller@radiantlogic.com>
2025-06-11 09:20:37 -0400

    Working on arg parsing integration

--- src/main/java/com/radiantlogic/dataconnector/codegen/Runner.java
+++ src/main/java/com/radiantlogic/dataconnector/codegen/Runner.java
@@ -1,27 +1,19 @@
 package com.radiantlogic.dataconnector.codegen;
 
-import com.radiantlogic.dataconnector.codegen.generate.CodeGenerator;
-import com.radiantlogic.dataconnector.codegen.generate.ConfigParser;
-import com.radiantlogic.dataconnector.codegen.generate.ParsedConfig;
+import com.radiantlogic.dataconnector.codegen.args.Args;
+import com.radiantlogic.dataconnector.codegen.args.ArgsParser;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class Runner {
-  private static final String COMMAND_GENERATE = "generate";
-
   public static void main(final String[] args) {
     if (args.length == 0) {
       throw new IllegalArgumentException("Cannot execute with specifying the command");
     }
 
     log.info("Starting program");
-
-    if (COMMAND_GENERATE.equals(args[0])) {
-      final ParsedConfig config = new ConfigParser().parseArgs(args);
-      new CodeGenerator(config).generate();
-      return;
-    }
-
-    throw new IllegalArgumentException("Unknown command: %s".formatted(args[0]));
+    final ArgsParser argsParser = new ArgsParser();
+    final Args parsedArgs = argsParser.parse(args);
+    // TODO run program
   }
 }

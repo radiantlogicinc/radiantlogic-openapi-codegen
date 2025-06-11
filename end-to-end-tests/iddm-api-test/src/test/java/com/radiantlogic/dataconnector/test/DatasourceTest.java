e5ab54eef8a3da80484dee89b1add5b55472245c
Craig Miller <cmiller@radiantlogic.com>
2025-06-11 13:12:18 -0400

    Progress

--- end-to-end-tests/iddm-api-test/src/test/java/com/radiantlogic/dataconnector/test/DatasourceTest.java
+++ end-to-end-tests/iddm-api-test/src/test/java/com/radiantlogic/dataconnector/test/DatasourceTest.java
@@ -2,8 +2,10 @@ package com.radiantlogic.dataconnector.test;
 
 import com.radiantlogic.custom.dataconnector.api.AuthTokenApiApi;
 import com.radiantlogic.custom.dataconnector.invoker.ApiClient;
+import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
+import java.util.Properties;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 
@@ -11,12 +13,17 @@ public class DatasourceTest {
   @Test
   @SneakyThrows
   void itCanCreateAndReadDatasource() {
+    final Properties testProps = new Properties();
+    try (InputStream stream = getClass().getClassLoader().getResourceAsStream("test.properties")) {
+      testProps.load(stream);
+    }
+
     final ApiClient apiClient = new ApiClient();
-    apiClient.setBasePath("https://rlqa-usw2-craig.dev01.radiantlogic.io");
+    apiClient.setBasePath(testProps.getProperty("basePath"));
 
     final AuthTokenApiApi authTokenApiApi = new AuthTokenApiApi(apiClient);
-    final String username = System.getenv("IDDM_USERNAME");
-    final String password = System.getenv("IDDM_PASSWORD");
+    final String username = testProps.getProperty("username");
+    final String password = testProps.getProperty("password");
     final String auth =
         Base64.getEncoder()
             .encodeToString(

faef62d3eb8edf0d6e199af055b382c6f8678b60
Craig Miller <cmiller@radiantlogic.com>
2025-06-11 09:54:21 -0400

    Implemented test

--- src/test/java/com/radiantlogic/dataconnector/codegen/args/ArgsParserTest.java
+++ src/test/java/com/radiantlogic/dataconnector/codegen/args/ArgsParserTest.java
@@ -1,11 +1,20 @@
 package com.radiantlogic.dataconnector.codegen.args;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.radiantlogic.dataconnector.codegen.properties.Props;
 import org.junit.jupiter.api.Test;
 
 public class ArgsParserTest {
+  final Props props = new Props("my-artifact", "1.0.0");
+  private final ArgsParser argsParser = new ArgsParser(props);
+
   @Test
   void itShowsHelpMenu() {
-    throw new RuntimeException();
+    final String[] args = {"-h"};
+    final Args parsedArgs = argsParser.parse(args);
+    final Args expectedArgs = new Args(ProgramArgStatus.EXIT, "", "", false);
+    assertThat(parsedArgs).isEqualTo(expectedArgs);
   }
 
   @Test

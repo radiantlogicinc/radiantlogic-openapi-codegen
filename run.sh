ba847bf2b005a97e50c657eac66ae5656831a1c2
Craig Miller <cmiller@radiantlogic.com>
2025-06-11 09:41:46 -0400

    Updating run config

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 mvn clean compile \
   exec:exec@generate \
-  -DopenapiPath=$(pwd)/radiantone-openapi-8.1.4-beta.2-SNAPSHOT.yaml
+  -Dargs="-p=$(pwd)/radiantone-openapi-8.1.4-beta.2-SNAPSHOT.yaml"

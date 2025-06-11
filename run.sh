52cf661b50c66f95454acf4fd8acb23ead7c1d50
Craig Miller <cmiller@radiantlogic.com>
2025-06-11 10:55:26 -0400

    Updated run script

--- run.sh
+++ run.sh
@@ -4,5 +4,4 @@ set -euo pipefail
 
 mvn clean compile \
   exec:exec@generate \
-  -DprogramArgs='-h'
-#  -DprogramArgs="-p=$(pwd)/radiantone-openapi-8.1.4-beta.2-SNAPSHOT.yaml"
+  -DprogramArgs="-p=$(pwd)/radiantone-openapi-8.1.4-beta.2-SNAPSHOT.yaml"

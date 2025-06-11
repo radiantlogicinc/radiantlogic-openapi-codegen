8623086cb6d993a3258f84c6926874ea3caef7bd
Craig Miller <cmiller@radiantlogic.com>
2025-06-11 09:24:46 -0400

    Implemented PropsReader

---
+++ src/main/java/com/radiantlogic/dataconnector/codegen/properties/Props.java
@@ -0,0 +1,5 @@
+package com.radiantlogic.dataconnector.codegen.properties;
+
+import lombok.NonNull;
+
+public record Props(@NonNull String artifactId, @NonNull String version) {}
---
+++ src/main/java/com/radiantlogic/dataconnector/codegen/properties/PropsReader.java
@@ -0,0 +1,16 @@
+package com.radiantlogic.dataconnector.codegen.properties;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class PropsReader {
+  public Props readProps() throws IOException {
+    try (InputStream stream =
+        getClass().getClassLoader().getResourceAsStream("project.properties")) {
+      final Properties props = new Properties();
+      props.load(stream);
+      return new Props(props.getProperty("artifactId"), props.getProperty("version"));
+    }
+  }
+}

73c4ccbd654a069fb1725e760261a50ba37615b9
Craig Miller <cmiller@radiantlogic.com>
2025-06-11 12:49:23 -0400

    Progress

--- end-to-end-tests/iddm-api-test/src/test/java/com/radiantlogic/dataconnector/test/DatasourceTest.java
+++ end-to-end-tests/iddm-api-test/src/test/java/com/radiantlogic/dataconnector/test/DatasourceTest.java
@@ -1,10 +1,12 @@
 package com.radiantlogic.dataconnector.test;
 
 import org.junit.jupiter.api.Test;
+import org.openapitools.client.ApiClient;
 
 public class DatasourceTest {
     @Test
     void itCanCreateAndReadDatasource() {
+        final ApiClient apiClient = new ApiClient();
         throw new RuntimeException();
     }
 }

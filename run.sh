18de1f964ad2a187d33489e372c060e5bc5eb6b9
Craig Miller <cmiller@radiantlogic.com>
2025-06-12 14:06:04 -0400

    Updated script

--- run.sh
+++ run.sh
@@ -11,6 +11,18 @@ generate() {
     -DprogramArgs="-p=$yaml"
 }
 
+install() {
+  local path
+  path="$1"
+  (
+    cd "$path"
+    mvn clean install -DskipTests
+  )
+}
+
 generate "$(pwd)/examples/okta/idp-minimal.yaml"
 #generate "$(pwd)/examples/okta/management-minimal.yaml"
 generate "$(pwd)/examples/okta/oauth-minimal.yaml"
+
+install ./rest-api-java-client-builder/output/MyAccount-Management/2025.01.1
+install ./rest-api-java-client-builder/output/Okta-OpenID-Connect-&-OAuth-2.0/2025.01.1

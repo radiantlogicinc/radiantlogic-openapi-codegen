645b46f65e203f6c3f38fb79f9a036055a7bbfe4
Craig Miller <cmiller@radiantlogic.com>
2025-06-12 12:20:48 -0400

    Progress

--- run.sh
+++ run.sh
@@ -4,9 +4,11 @@ set -euo pipefail
 
 mvn clean compile \
   exec:exec@generate \
-  -DprogramArgs="-p=$(pwd)/end-to-end-tests/iddm-api-test/radiantone-openapi-8.1.4-beta.2-SNAPSHOT.yaml"
+  -DprogramArgs="-p=$(pwd)/idp-minimal.yaml"
+#  -DprogramArgs="-p=$(pwd)/end-to-end-tests/iddm-api-test/radiantone-openapi-8.1.4-beta.2-SNAPSHOT.yaml"
 
 (
-  cd ./rest-api-java-client-builder/output/RadiantOne-V8-API/8.1.4-beta.2-SNAPSHOT
+#  cd ./rest-api-java-client-builder/output/RadiantOne-V8-API/8.1.4-beta.2-SNAPSHOT
+  cd ./rest-api-java-client-builder/output/MyAccount-Management/2025.01.1
   mvn clean install -DskipTests
 )

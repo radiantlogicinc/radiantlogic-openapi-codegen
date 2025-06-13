d270c48236f6b4e4b34f66a4ac30f4c733bc0c32
Craig Miller <cmiller@radiantlogic.com>
2025-06-13 10:03:27 -0400

    Adjusting script

--- run.sh
+++ run.sh
@@ -20,10 +20,11 @@ install() {
   )
 }
 
-#generate "$(pwd)/examples/okta/idp-minimal.yaml"
-generate "$(pwd)/examples/okta/management-minimal.yaml"
-#generate "$(pwd)/examples/okta/oauth-minimal.yaml"
+generate "$(pwd)/examples/okta/idp-minimal.yaml"
+install ./rest-api-java-client-builder/output/MyAccount-Management/2025.01.1
 
-#install ./rest-api-java-client-builder/output/MyAccount-Management/2025.01.1
+generate "$(pwd)/examples/okta/management-minimal.yaml"
 install ./rest-api-java-client-builder/output/Okta-Admin-Management/2025.01.1
-#install ./rest-api-java-client-builder/output/Okta-OpenID-Connect--OAuth-2.0/2025.01.1
+
+generate "$(pwd)/examples/okta/oauth-minimal.yaml"
+install ./rest-api-java-client-builder/output/Okta-OpenID-Connect--OAuth-2.0/2025.01.1

62b4d692953a6469be648171e11a81b9e5a82a45
Craig Miller <cmiller@radiantlogic.com>
2025-06-13 08:30:44 -0400

    Copied code

--- rest-api-java-client-builder/src/main/java/com/radiantlogic/dataconnector/codegen/generate/DataconnectorJavaClientCodegen.java
+++ rest-api-java-client-builder/src/main/java/com/radiantlogic/dataconnector/codegen/generate/DataconnectorJavaClientCodegen.java
@@ -5,16 +5,23 @@ import com.radiantlogic.dataconnector.codegen.io.CodegenPaths;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 import lombok.NonNull;
 import org.apache.commons.io.FileUtils;
+import org.openapitools.codegen.CodegenDiscriminator;
 import org.openapitools.codegen.CodegenModel;
 import org.openapitools.codegen.CodegenProperty;
 import org.openapitools.codegen.languages.JavaClientCodegen;
+import org.openapitools.codegen.model.ModelsMap;
+import org.openapitools.codegen.utils.ModelUtils;
 
 /**
  * A customized version of the default JavaClientCodegen designed to produce the exact artifact
@@ -119,5 +126,46 @@ public class DataconnectorJavaClientCodegen extends JavaClientCodegen {
       codegenModel.vars = codegenProperties;
     }
     return codegenModel;
+  }
+
+  // TODO cleanup or delete
+  @Override
+  public Map<String, ModelsMap> postProcessAllModels(final Map<String, ModelsMap> objs) {
+    objs.keySet().stream()
+        .map(key -> ModelUtils.getModelByName(key, objs))
+        .filter(
+            model -> model.discriminator != null && model.discriminator.getMappedModels() != null)
+        .forEach(
+            model -> {
+              final Set<CodegenDiscriminator.MappedModel> mappedModels =
+                  model.discriminator.getMappedModels().stream()
+                      .filter(this::isExplicitMapping)
+                      .map(
+                          mappedModel -> {
+                            final CodegenModel codegenMappedModel =
+                                ModelUtils.getModelByName(mappedModel.getModelName(), objs);
+
+                            //
+                            // codegenMappedModel.setParent(model.getClassname());
+                            reconcileInlineEnums(codegenMappedModel, model);
+                            return mappedModel;
+                          })
+                      .collect(Collectors.toSet());
+
+              model.getDiscriminator().setMappedModels(mappedModels);
+            });
+
+    return super.postProcessAllModels(objs);
+  }
+
+  private boolean isExplicitMapping(@NonNull final CodegenDiscriminator.MappedModel mappedModel) {
+    try {
+      final Field field =
+          CodegenDiscriminator.MappedModel.class.getDeclaredField("explicitMapping");
+      field.setAccessible(true);
+      return (boolean) field.get(mappedModel);
+    } catch (final ReflectiveOperationException ex) {
+      throw new RuntimeException("Failed to check for an explicit discriminator mapping", ex);
+    }
   }
 }

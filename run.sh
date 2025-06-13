fcd45bd3c3a018b51886b7b16db632964b9056c1
Craig Miller <cmiller@radiantlogic.com>
2025-06-13 10:04:38 -0400

    Updated script

--- run.sh
+++ run.sh
@@ -30,3 +30,4 @@ install() {
 #install ./rest-api-java-client-builder/output/Okta-OpenID-Connect--OAuth-2.0/2025.01.1
 
 generate "$(pwd)/examples/iddm/radiantone-openapi-8.1.4-beta.2-SNAPSHOT.yaml"
+install ./rest-api-java-client-builder/output/RadiantOne-V8-API/8.1.4-beta.2-SNAPSHOT

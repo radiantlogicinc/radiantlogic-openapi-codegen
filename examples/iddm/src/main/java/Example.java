6c964d9e571c3bd6d7ab1f07b0c787e5e5eb4b2f
Craig Miller <cmiller@radiantlogic.com>
2025-06-13 19:02:38 -0400

    Fixing password

--- examples/iddm/src/main/java/Example.java
+++ examples/iddm/src/main/java/Example.java
@@ -13,7 +13,7 @@ public class Example {
     apiClient.setDebugging(true);
 
     final AuthTokenApiApi authTokenApiApi = new AuthTokenApiApi(apiClient);
-    final String basicAuth = "%s:%s".formatted("cn=directory manager", "kJ3UmB6KDYt12gr!");
+    final String basicAuth = "%s:%s".formatted("cn=directory manager", "PASSWORD");
     final String base64EncodedAuth = Base64.getEncoder().encodeToString(basicAuth.getBytes());
     final String token =
         authTokenApiApi.postLogin("Basic %s".formatted(base64EncodedAuth)).getToken();

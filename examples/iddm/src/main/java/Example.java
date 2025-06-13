c9becf3076f459b0c81c4e641dee8dcf4f68de83
Craig Miller <cmiller@radiantlogic.com>
2025-06-13 17:36:34 -0400

    Progress

--- examples/iddm/src/main/java/Example.java
+++ examples/iddm/src/main/java/Example.java
@@ -2,15 +2,17 @@ import com.radiantlogic.custom.dataconnector.api.AuthTokenApiApi;
 import com.radiantlogic.custom.dataconnector.api.DataSourcesApi;
 import com.radiantlogic.custom.dataconnector.invoker.ApiClient;
 import com.radiantlogic.custom.dataconnector.model.LdapDataSource;
+import com.radiantlogic.custom.dataconnector.model.RequiredDataSourceCategory;
 import java.util.Base64;
 
 public class Example {
   public static void main(final String[] args) throws Exception {
     final ApiClient apiClient = new ApiClient();
     apiClient.setBasePath("https://rlqa-usw2-craig.dev01.radiantlogic.io/api");
+    apiClient.setDebugging(true);
 
     final AuthTokenApiApi authTokenApiApi = new AuthTokenApiApi(apiClient);
-    final String basicAuth = "%s:%s".formatted("username", "password");
+    final String basicAuth = "%s:%s".formatted("cn=directory manager", "");
     final String base64EncodedAuth = Base64.getEncoder().encodeToString(basicAuth.getBytes());
     final String token =
         authTokenApiApi.postLogin("Basic %s".formatted(base64EncodedAuth)).getToken();
@@ -21,6 +23,8 @@ public class Example {
     final LdapDataSource ldapDataSource = new LdapDataSource();
     ldapDataSource.setHost("localhost");
     ldapDataSource.setPort(389);
+    ldapDataSource.setCategory(RequiredDataSourceCategory.LDAP);
+    ldapDataSource.setType("Active Directory");
     dataSourcesApi.createDataSource(ldapDataSource);
   }
 }

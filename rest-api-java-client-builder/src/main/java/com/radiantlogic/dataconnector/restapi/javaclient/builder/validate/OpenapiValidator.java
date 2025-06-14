44e3ffad8dad576f01734a050caf278034e7da7a
Craig Miller <cmiller@radiantlogic.com>
2025-06-14 15:52:16 -0400

    Working on validator

---
+++ rest-api-java-client-builder/src/main/java/com/radiantlogic/dataconnector/restapi/javaclient/builder/validate/OpenapiValidator.java
@@ -0,0 +1,10 @@
+package com.radiantlogic.dataconnector.restapi.javaclient.builder.validate;
+
+import com.radiantlogic.dataconnector.restapi.javaclient.builder.args.Args;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class OpenapiValidator {
+  public void validate(@NonNull final Args args) {}
+}

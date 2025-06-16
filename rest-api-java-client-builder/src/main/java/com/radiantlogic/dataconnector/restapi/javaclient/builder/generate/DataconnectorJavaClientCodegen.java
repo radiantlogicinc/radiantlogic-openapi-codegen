ca074c9b28e46e1fc941851730fda53f7f15357e
Craig Miller <cmiller@radiantlogic.com>
2025-06-16 09:53:40 -0400

    Added code to fix packages that have elements beginning with a number

--- rest-api-java-client-builder/src/main/java/com/radiantlogic/dataconnector/restapi/javaclient/builder/generate/DataconnectorJavaClientCodegen.java
+++ rest-api-java-client-builder/src/main/java/com/radiantlogic/dataconnector/restapi/javaclient/builder/generate/DataconnectorJavaClientCodegen.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.media.Schema;
 import java.nio.file.Path;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
@@ -52,6 +53,29 @@ public class DataconnectorJavaClientCodegen extends JavaClientCodegen {
 
   private static String ensureValidPackageName(@NonNull final String packageName) {
     return packageName.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+  }
+
+  private static String fixLeadingNumbers(final String packageName) {
+    return Arrays.stream(packageName.split("\\."))
+        .map(
+            name -> {
+              final String beginning =
+                  switch (name.charAt('0')) {
+                    case '0' -> "zero";
+                    case '1' -> "one";
+                    case '2' -> "two";
+                    case '3' -> "three";
+                    case '4' -> "four";
+                    case '5' -> "five";
+                    case '6' -> "six";
+                    case '7' -> "seven";
+                    case '8' -> "eight";
+                    case '9' -> "nine";
+                    default -> "%s".formatted(name.charAt(0));
+                  };
+              return "%s%s".formatted(beginning, name.substring(1));
+            })
+        .collect(Collectors.joining("."));
   }
 
   private void init(@NonNull final Args args) {

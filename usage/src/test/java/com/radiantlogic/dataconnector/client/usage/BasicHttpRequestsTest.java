63b0c9faa98a153092411f6124e804e9934d27a4
Craig Miller <cmiller@radiantlogic.com>
2025-06-17 11:34:17 -0400

    Refactoring

---
+++ usage/src/test/java/com/radiantlogic/dataconnector/client/usage/BasicHttpRequestsTest.java
@@ -0,0 +1,12 @@
+package com.radiantlogic.dataconnector.client.usage;
+
+import com.github.tomakehurst.wiremock.junit5.WireMockTest;
+import org.junit.jupiter.api.Test;
+
+@WireMockTest(httpPort = 9000)
+public class BasicHttpRequestsTest {
+  @Test
+  void test() {
+    throw new RuntimeException();
+  }
+}

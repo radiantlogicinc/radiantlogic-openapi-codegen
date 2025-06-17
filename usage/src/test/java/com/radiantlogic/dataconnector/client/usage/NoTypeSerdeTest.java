48f5d09f195be38a8f44d58e2ab33bc7b29612b1
Craig Miller <cmiller@radiantlogic.com>
2025-06-17 10:50:39 -0400

    Implemented test

--- usage/src/test/java/com/radiantlogic/dataconnector/client/usage/NoTypeSerdeTest.java
+++ usage/src/test/java/com/radiantlogic/dataconnector/client/usage/NoTypeSerdeTest.java
@@ -1,9 +1,13 @@
 package com.radiantlogic.dataconnector.client.usage;
 
+import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.radiantlogic.custom.dataconnector.openaiapi.model.CreateCompletionRequest;
 import java.math.BigDecimal;
 import java.util.Arrays;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -14,6 +18,7 @@ public class NoTypeSerdeTest {
   private static final ObjectMapper objectMapper = new ObjectMapper();
 
   @Test
+  @SneakyThrows
   void openaiCreateCompletionRequest() {
     final CreateCompletionRequest request = new CreateCompletionRequest();
     request.setBestOf(1);
@@ -23,7 +28,13 @@ public class NoTypeSerdeTest {
     request.setPrompt(Arrays.asList("hello", "world"));
     request.setStop(Arrays.asList(1, 2, 3));
 
-    final String json = ResourceReader.read("data/notypeserde/create-completion-request.json");
-    throw new RuntimeException();
+    final String expectedJson =
+        ResourceReader.read("data/notypeserde/create-completion-request.json");
+    final String actualJson = objectMapper.writeValueAsString(request);
+    assertThatJson(actualJson).isEqualTo(expectedJson);
+
+    final CreateCompletionRequest actualRequest =
+        objectMapper.readValue(actualJson, CreateCompletionRequest.class);
+    assertThat(actualRequest).usingRecursiveComparison().isEqualTo(request);
   }
 }

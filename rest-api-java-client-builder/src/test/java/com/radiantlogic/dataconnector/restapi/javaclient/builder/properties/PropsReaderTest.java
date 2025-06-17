24f0d67b982435036188889b0ca15c78f8d3448d
craig-miller-radiant <cmiller@radiantlogic.com>
2025-06-17 15:03:10 -0400

    Added a suite of tests to validate the usage of the generated code (#21)
    * Preparing to work on tests
    
    * Working on tests
    
    * Preparing tests
    
    * Progress with tests
    
    * Updated pre-commit script
    
    * Progress
    
    * Progress with tests
    
    * Progress
    
    * Working on resources
    
    * Working on tests
    
    * Adding test data
    
    * Updated tests
    
    * Updated test
    
    * Fixing json
    
    * Added lib
    
    * Got test to pass
    
    * Cleanup
    
    * Got all serde tests passing
    
    * Working on another test
    
    * Working on tests
    
    * More progress with test
    
    * Updated tests
    
    * Small test refactoring
    
    * Adding test data
    
    * More test progress
    
    * Updated test data
    
    * Working on more tests
    
    * Progress with tests
    
    * Refactoring tests
    
    * Working on test
    
    * Implemented test
    
    * Setup basic model
    
    * Updated test data
    
    * Notes cleanup
    
    * Test cleanup
    
    * Refactoring
    
    * Progress
    
    * Copying logging config
    
    * More libs
    
    * Progress with configuration
    
    * Setting up radiantone client
    
    * Working on test
    
    * Fixing test
    
    * Progress
    
    * Progress-ish
    
    * Progress
    
    * Cleanup
    
    * Refactored tests
    
    * Progress
    
    * Refactoring
    
    * More fixes to tests
    
    * Progress
    
    * More progress
    
    * Progress with tests
    
    * Progress
    
    * Refactoring streams
    
    * More resource refactoring
    
    * Added bad ai generated image file
    
    * Got the refactored file working
    
    * More refactoring
    
    * Fixing assertion
    
    * Working on multipart
    
    * Progress
    
    * Updated test
    
    * Progress
    
    * Cleanup
    
    * Progress
    
    * Refactoring
    
    * More refactoring
    
    * More refactoring
    
    * More refactoring
    
    * more refactoring
    
    * Updated test
    
    * Improved test
    
    * Progress
    
    * Fixing test setup
    
    * Got test to pass

--- rest-api-java-client-builder/src/test/java/com/radiantlogic/dataconnector/restapi/javaclient/builder/properties/PropsReaderTest.java
+++ rest-api-java-client-builder/src/test/java/com/radiantlogic/dataconnector/restapi/javaclient/builder/properties/PropsReaderTest.java
@@ -5,7 +5,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.stream.Stream;
+import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import lombok.NonNull;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -22,17 +25,40 @@ public class PropsReaderTest {
   @SneakyThrows
   static void beforeAll() {
     final Path pomXmlPath = Path.of(System.getProperty("user.dir"), "pom.xml");
-    final Document doc;
+    final Path parentPomXmlPath = Path.of(System.getProperty("user.dir"), "..", "pom.xml");
+
+    final DocumentBuilder docBuilder =
+        DocumentBuilderFactory.newDefaultInstance().newDocumentBuilder();
+
+    final Document pomDoc;
     try (InputStream stream = Files.newInputStream(pomXmlPath)) {
-      doc = DocumentBuilderFactory.newDefaultInstance().newDocumentBuilder().parse(stream);
+      pomDoc = docBuilder.parse(stream);
     }
-    final Element root = doc.getDocumentElement();
-    artifactId = getSingleElement(root, "artifactId").getTextContent();
-    version = getSingleElement(root, "version").getTextContent();
+    artifactId = getSingleElement(pomDoc.getDocumentElement(), "artifactId").getTextContent();
+
+    final Document parentPomDoc;
+    try (InputStream stream = Files.newInputStream(parentPomXmlPath)) {
+      parentPomDoc = docBuilder.parse(stream);
+    }
+
+    final Element propsElement = getSingleElement(parentPomDoc.getDocumentElement(), "properties");
+    version = getSingleElement(propsElement, "revision").getTextContent();
   }
 
-  private static Element getSingleElement(final Element base, final String tagName) {
-    return (Element) base.getElementsByTagName(tagName).item(0);
+  private static Element getSingleElement(
+      @NonNull final Element base, @NonNull final String tagName) {
+    return getElementStream(base)
+        .filter(elem -> elem.getTagName().equals(tagName))
+        .findFirst()
+        .orElseThrow();
+  }
+
+  private static Stream<Element> getElementStream(@NonNull final Element base) {
+    final var nodeList = base.getChildNodes();
+    return Stream.iterate(0, i -> i < nodeList.getLength(), i -> i + 1)
+        .map(nodeList::item)
+        .filter(item -> item.getNodeType() == Element.ELEMENT_NODE)
+        .map(Element.class::cast);
   }
 
   /**
---
+++ usage/src/test/java/com/radiantlogic/dataconnector/client/usage/ResourceReader.java
@@ -0,0 +1,47 @@
+package com.radiantlogic.dataconnector.client.usage;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.SneakyThrows;
+import org.apache.commons.io.IOUtils;
+import org.springframework.lang.NonNull;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ResourceReader {
+  @SneakyThrows
+  public static String readString(@NonNull final String resourceName) {
+    try (InputStream stream = openStream(resourceName)) {
+      return IOUtils.toString(stream, StandardCharsets.UTF_8);
+    }
+  }
+
+  private static URL getResourceUrl(@NonNull final String resourceName) {
+    final URL url = ResourceReader.class.getClassLoader().getResource(resourceName);
+    if (url == null) {
+      throw new IllegalArgumentException(String.format("Resource not found: %s", resourceName));
+    }
+    return url;
+  }
+
+  @SneakyThrows
+  private static InputStream openStream(@NonNull final String resourceName) {
+    return getResourceUrl(resourceName).openStream();
+  }
+
+  @SneakyThrows
+  public static byte[] readBytes(@NonNull final String resourceName) {
+    try (InputStream stream = openStream(resourceName)) {
+      return IOUtils.toByteArray(stream);
+    }
+  }
+
+  @SneakyThrows
+  public static Path getFilePath(@NonNull final String resourceName) {
+    return Paths.get(getResourceUrl(resourceName).toURI());
+  }
+}

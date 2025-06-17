b6f87a45ef19ab9d67577929ed71f9504139ec15
Craig Miller <cmiller@radiantlogic.com>
2025-06-17 10:46:58 -0400

    Working on test

--- usage/src/test/java/com/radiantlogic/dataconnector/client/usage/NoTypeSerdeTest.java
+++ usage/src/test/java/com/radiantlogic/dataconnector/client/usage/NoTypeSerdeTest.java
@@ -2,6 +2,8 @@ package com.radiantlogic.dataconnector.client.usage;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.radiantlogic.custom.dataconnector.openaiapi.model.CreateCompletionRequest;
+import java.math.BigDecimal;
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -14,6 +16,12 @@ public class NoTypeSerdeTest {
   @Test
   void openaiCreateCompletionRequest() {
     final CreateCompletionRequest request = new CreateCompletionRequest();
+    request.setBestOf(1);
+    request.setEcho(true);
+    request.setPresencePenalty(new BigDecimal("10"));
+    request.setModel("o3");
+    request.setPrompt(Arrays.asList("hello", "world"));
+    request.setStop(Arrays.asList(1, 2, 3));
 
     final String json = ResourceReader.read("data/notypeserde/create-completion-request.json");
     throw new RuntimeException();

0ab66f8d33a2ddaf7666679ebd3c19040b9dc9df
Craig Miller <cmiller@radiantlogic.com>
2025-06-17 15:33:52 -0400

    Removed pre processing

--- rest-api-java-client-builder/src/main/java/com/radiantlogic/dataconnector/restapi/javaclient/builder/generate/CodeGenerator.java
+++ rest-api-java-client-builder/src/main/java/com/radiantlogic/dataconnector/restapi/javaclient/builder/generate/CodeGenerator.java
@@ -2,15 +2,12 @@ package com.radiantlogic.dataconnector.restapi.javaclient.builder.generate;
 
 import com.radiantlogic.dataconnector.restapi.javaclient.builder.args.Args;
 import io.swagger.parser.OpenAPIParser;
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.parser.core.models.ParseOptions;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -41,8 +38,6 @@ public class CodeGenerator {
       throw new IllegalStateException(
           "Failed to parse OpenAPI specification, see logs for details");
     }
-
-    preProcessOpenAPI(openAPI);
 
     log.debug("Performing code generation");
     final DataconnectorJavaClientCodegen codegen =
@@ -78,15 +73,5 @@ public class CodeGenerator {
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
-  }
-
-  /** Perform any necessary pre-processing to resolve potential codegen issues. */
-  private void preProcessOpenAPI(@NonNull final OpenAPI openAPI) {
-    log.debug("Pre-processing OpenAPI");
-    Optional.ofNullable(openAPI.getComponents())
-        .map(Components::getSchemas)
-        .orElseGet(Map::of)
-        // Clear the title to prevent accidental identification of duplicate schemas
-        .forEach((key, schema) -> schema.setTitle(null));
   }
 }

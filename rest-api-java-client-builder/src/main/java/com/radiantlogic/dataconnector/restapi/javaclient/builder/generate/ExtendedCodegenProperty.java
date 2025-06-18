dd731ac27ca8a59daf75676e3e5e6e442699a149
Craig Miller <cmiller@radiantlogic.com>
2025-06-18 12:59:12 -0400

    Progress with new property

--- rest-api-java-client-builder/src/main/java/com/radiantlogic/dataconnector/restapi/javaclient/builder/generate/ExtendedCodegenProperty.java
+++ rest-api-java-client-builder/src/main/java/com/radiantlogic/dataconnector/restapi/javaclient/builder/generate/ExtendedCodegenProperty.java
@@ -1,5 +1,13 @@
 package com.radiantlogic.dataconnector.restapi.javaclient.builder.generate;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 import org.openapitools.codegen.CodegenProperty;
 
-public class ExtendedCodegenProperty extends CodegenProperty {}
+@Getter
+@Setter
+@EqualsAndHashCode(callSuper = true)
+public class ExtendedCodegenProperty extends CodegenProperty {
+  public String jsonName;
+}

3eb8f34890c6952ca17aebee204bb90303c63024
Craig Miller <cmiller@radiantlogic.com>
2025-06-20 16:35:48 -0400

    Setting up tests

--- rest-api-java-client-builder/src/test/java/com/radiantlogic/dataconnector/restapi/javaclient/builder/generate/codegen/support/CodegenMetadataSupportTest.java
+++ rest-api-java-client-builder/src/test/java/com/radiantlogic/dataconnector/restapi/javaclient/builder/generate/codegen/support/CodegenMetadataSupportTest.java
@@ -1,8 +1,15 @@
 package com.radiantlogic.dataconnector.restapi.javaclient.builder.generate.codegen.support;
 
+import com.radiantlogic.dataconnector.restapi.javaclient.builder.args.Args;
+import com.radiantlogic.dataconnector.restapi.javaclient.builder.args.ProgramArgStatus;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.junit.jupiter.api.Test;
 
 public class CodegenMetadataSupportTest {
+  private final CodegenMetadataSupport codegenMetadataSupport = new CodegenMetadataSupport();
+  private final Args args = new Args(ProgramArgStatus.PROCEED, "/foo/bar.yaml", "com.something");
+
   @Test
   void itGetsSimpleMetadata() {
     throw new RuntimeException();
@@ -21,5 +28,11 @@ public class CodegenMetadataSupportTest {
   @Test
   void itGetsMetadataWithTitleWithLeadingNumbers() {
     throw new RuntimeException();
+  }
+
+  private OpenAPI createOpenAPI() {
+    final OpenAPI openAPI = new OpenAPI();
+    openAPI.setInfo(new Info());
+    return openAPI;
   }
 }

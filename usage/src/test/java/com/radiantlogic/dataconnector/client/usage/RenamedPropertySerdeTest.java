da8cdfed798c0e7c94c5f8643b4a98637a5d064a
Craig Miller <cmiller@radiantlogic.com>
2025-06-20 15:12:03 -0400

    Stubbed out test

---
+++ usage/src/test/java/com/radiantlogic/dataconnector/client/usage/RenamedPropertySerdeTest.java
@@ -0,0 +1,7 @@
+package com.radiantlogic.dataconnector.client.usage;
+
+/**
+ * A test class that validates that an object with properties that are renamed between the JSON and
+ * POJO will serialize/deserialize.
+ */
+public class RenamedPropertySerdeTest {}

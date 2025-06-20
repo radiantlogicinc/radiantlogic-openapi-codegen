39e9e704847568db7e263a7421764e542e404510
Craig Miller <cmiller@radiantlogic.com>
2025-06-20 16:38:51 -0400

    Implemented test

--- rest-api-java-client-builder/src/test/java/com/radiantlogic/dataconnector/restapi/javaclient/builder/generate/codegen/support/CodegenMetadataSupportTest.java
+++ rest-api-java-client-builder/src/test/java/com/radiantlogic/dataconnector/restapi/javaclient/builder/generate/codegen/support/CodegenMetadataSupportTest.java
@@ -1,7 +1,10 @@
 package com.radiantlogic.dataconnector.restapi.javaclient.builder.generate.codegen.support;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import com.radiantlogic.dataconnector.restapi.javaclient.builder.args.Args;
 import com.radiantlogic.dataconnector.restapi.javaclient.builder.args.ProgramArgStatus;
+import com.radiantlogic.dataconnector.restapi.javaclient.builder.io.CodegenPaths;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.junit.jupiter.api.Test;
@@ -12,7 +15,17 @@ public class CodegenMetadataSupportTest {
 
   @Test
   void itGetsSimpleMetadata() {
-    throw new RuntimeException();
+    final OpenAPI openAPI = createOpenAPI();
+    openAPI.getInfo().setTitle("MyTitle");
+    openAPI.getInfo().setVersion("1.0.0");
+    final var metadata = codegenMetadataSupport.getMetadata(openAPI, args);
+    final var expectedMetadata =
+        new CodegenMetadataSupport.CodegenMetadata(
+            CodegenPaths.OUTPUT_DIR.resolve("MyTitle").resolve("1.0.0"),
+            "MyTitle",
+            "1.0.0",
+            "com.something.mytitle");
+    assertThat(metadata).isEqualTo(expectedMetadata);
   }
 
   @Test

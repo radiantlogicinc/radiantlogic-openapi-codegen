892605a9c7d208cb014fbc4fd40db5057ca87ec3
Craig Miller <cmiller@radiantlogic.com>
2025-06-23 15:44:46 -0400

    Progress

--- rest-api-java-client-builder/src/main/java/com/radiantlogic/dataconnector/restapi/javaclient/builder/generate/codegen/support/JavaCodegenDecorator.java
+++ rest-api-java-client-builder/src/main/java/com/radiantlogic/dataconnector/restapi/javaclient/builder/generate/codegen/support/JavaCodegenDecorator.java
@@ -9,7 +9,7 @@ import org.openapitools.codegen.languages.AbstractJavaCodegen;
  * A decorator that wraps around an implementation of a Java codegen. This applies enhancements to
  * how the codegen operates. The benefits to this decorator approach rather than extending is that
  * common changes that could apply to multiple Java codegens (server & client) can be shared easily
- * with this delegate.
+ * with this decorator.
  */
 @RequiredArgsConstructor
 public class JavaCodegenDecorator implements CodegenConfig {

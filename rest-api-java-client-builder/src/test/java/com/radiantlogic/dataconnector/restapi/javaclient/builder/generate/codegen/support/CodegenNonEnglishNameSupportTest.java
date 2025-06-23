0da4d9857433b1d0a064e917d5e39d5040934c68
Craig Miller <cmiller@radiantlogic.com>
2025-06-23 15:48:58 -0400

    Stubbed out more tests

---
+++ rest-api-java-client-builder/src/test/java/com/radiantlogic/dataconnector/restapi/javaclient/builder/generate/codegen/support/CodegenNonEnglishNameSupportTest.java
@@ -0,0 +1,20 @@
+package com.radiantlogic.dataconnector.restapi.javaclient.builder.generate.codegen.support;
+
+import org.junit.jupiter.api.Test;
+
+public class CodegenNonEnglishNameSupportTest {
+  @Test
+  void itDoesNothingForValidOperationIds() {
+    throw new RuntimeException();
+  }
+
+  @Test
+  void itStripsNonEnglishOperationIdCharactersAndLeavesTheRest() {
+    throw new RuntimeException();
+  }
+
+  @Test
+  void itNullsOperationIdWithNoNonEnglishCharacters() {
+    throw new RuntimeException();
+  }
+}

1d3b06a2a7438f60b96ca565d7b279a4377c63a0
Craig Miller <cmiller@radiantlogic.com>
2025-06-23 16:02:07 -0400

    stubbed out tests

--- rest-api-java-client-builder/src/main/java/com/radiantlogic/dataconnector/restapi/javaclient/builder/generate/codegen/support/CodegenLiteralPropertyNameSupport.java
+++ rest-api-java-client-builder/src/main/java/com/radiantlogic/dataconnector/restapi/javaclient/builder/generate/codegen/support/CodegenLiteralPropertyNameSupport.java
@@ -7,9 +7,11 @@ import org.apache.commons.lang3.StringUtils;
 /** Valid JSON property names are not always valid Java field names. This corrects any problems. */
 public class CodegenLiteralPropertyNameSupport {
   public void fixBadNames(@NonNull final ExtendedCodegenProperty prop) {
-    if (prop.name != null && StringUtils.isNumeric(prop.name)
-        || "true".equals(prop.name)
-        || "false".equals(prop.name)) {
+    if (prop.name == null) {
+      return;
+    }
+
+    if (StringUtils.isNumeric(prop.name) || "true".equals(prop.name) || "false".equals(prop.name)) {
       prop.jsonName = prop.name;
       prop.name =
           "value%s%s"
---
+++ rest-api-java-client-builder/src/test/java/com/radiantlogic/dataconnector/restapi/javaclient/builder/generate/codegen/support/CodegenLiteralPropertyNameSupportTest.java
@@ -0,0 +1,25 @@
+package com.radiantlogic.dataconnector.restapi.javaclient.builder.generate.codegen.support;
+
+import org.junit.jupiter.api.Test;
+
+public class CodegenLiteralPropertyNameSupportTest {
+  @Test
+  void itDoesNothingWithValidName() {
+    throw new RuntimeException();
+  }
+
+  @Test
+  void itFixesNumericName() {
+    throw new RuntimeException();
+  }
+
+  @Test
+  void itFixesTrueName() {
+    throw new RuntimeException();
+  }
+
+  @Test
+  void itFixesFalseName() {
+    throw new RuntimeException();
+  }
+}

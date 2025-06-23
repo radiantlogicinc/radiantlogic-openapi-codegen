51ec68d5f5c0a2c93604e137a2561111636cb17d
Craig Miller <cmiller@radiantlogic.com>
2025-06-23 15:57:14 -0400

    Stubbed tests

---
+++ rest-api-java-client-builder/src/test/java/com/radiantlogic/dataconnector/restapi/javaclient/builder/generate/codegen/support/CodegenEnumValueOfSupportTest.java
@@ -0,0 +1,15 @@
+package com.radiantlogic.dataconnector.restapi.javaclient.builder.generate.codegen.support;
+
+import org.junit.jupiter.api.Test;
+
+public class CodegenEnumValueOfSupportTest {
+  @Test
+  void itHasStringDatatype() {
+    throw new RuntimeException();
+  }
+
+  @Test
+  void itHasBigDecimalDatatype() {
+    throw new RuntimeException();
+  }
+}

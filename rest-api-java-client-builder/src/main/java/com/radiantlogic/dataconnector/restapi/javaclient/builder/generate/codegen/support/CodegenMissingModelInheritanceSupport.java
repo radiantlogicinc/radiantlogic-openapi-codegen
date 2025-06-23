ac5bf6e8223430330bb979f8cf1e9b7ce78cb5ea
Craig Miller <cmiller@radiantlogic.com>
2025-06-23 16:07:47 -0400

    Working on comments

--- rest-api-java-client-builder/src/main/java/com/radiantlogic/dataconnector/restapi/javaclient/builder/generate/codegen/support/CodegenMissingModelInheritanceSupport.java
+++ rest-api-java-client-builder/src/main/java/com/radiantlogic/dataconnector/restapi/javaclient/builder/generate/codegen/support/CodegenMissingModelInheritanceSupport.java
@@ -2,6 +2,49 @@ package com.radiantlogic.dataconnector.restapi.javaclient.builder.generate.codeg
 
 /**
  * There are some scenarios where inheritance relationships that should exist between schemas don't
- * in an OpenAPI spec. This is mainly problematic for
+ * in an OpenAPI spec. This is mainly problematic for classes with inheritance that have fields with
+ * inheritance. Take this example:
+ *
+ * <p>``` public class Group { // ... }
+ *
+ * <p>public class Team extends Group { // ... }
+ *
+ * <p>public class Parent { private Group item; }
+ *
+ * <p>public class Child extends Parent { private Team item; } ```
+ *
+ * <p>As long as Team extends Group, everything will work. If Team does not extend Group, there will
+ * be a compile error. This support class exists to make sure that this kind of hierarchy will
+ * always work.
  */
-public class CodegenMissingModelInheritanceSupport {}
+public class CodegenMissingModelInheritanceSupport {
+
+  public static class Group {}
+
+  public static class Team extends Group {}
+
+  public static class Parent {
+    private Group item;
+
+    public Group getItem() {
+      return item;
+    }
+
+    public void setItem(Group item) {
+      this.item = item;
+    }
+  }
+
+  public static class Child extends Parent {
+    private Team item;
+
+    @Override
+    public Team getItem() {
+      return item;
+    }
+
+    public void setItem(Team item) {
+      this.item = item;
+    }
+  }
+}

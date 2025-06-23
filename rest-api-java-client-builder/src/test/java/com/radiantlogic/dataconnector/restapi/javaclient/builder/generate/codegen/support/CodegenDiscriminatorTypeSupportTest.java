274563363060079b0d1ff552eb296bd8d60b49ca
Craig Miller <cmiller@radiantlogic.com>
2025-06-23 15:37:02 -0400

    Stubbed out tests

---
+++ rest-api-java-client-builder/src/test/java/com/radiantlogic/dataconnector/restapi/javaclient/builder/generate/codegen/support/CodegenDiscriminatorTypeSupportTest.java
@@ -0,0 +1,20 @@
+package com.radiantlogic.dataconnector.restapi.javaclient.builder.generate.codegen.support;
+
+import org.junit.jupiter.api.Test;
+
+public class CodegenDiscriminatorTypeSupportTest {
+  @Test
+  void testItHasNoDiscriminator() {
+    throw new RuntimeException();
+  }
+
+  @Test
+  void testItHasDiscriminatorWithCorrectType() {
+    throw new RuntimeException();
+  }
+
+  @Test
+  void testItHasDiscriminatorWithIncorrectType() {
+    throw new RuntimeException();
+  }
+}

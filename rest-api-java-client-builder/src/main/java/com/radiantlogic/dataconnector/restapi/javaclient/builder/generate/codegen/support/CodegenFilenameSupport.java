c008f147c115cf330ee6bbc7d63b02ac9b439512
Craig Miller <cmiller@radiantlogic.com>
2025-06-24 14:44:37 -0400

    Progress

--- rest-api-java-client-builder/src/main/java/com/radiantlogic/dataconnector/restapi/javaclient/builder/generate/codegen/support/CodegenFilenameSupport.java
+++ rest-api-java-client-builder/src/main/java/com/radiantlogic/dataconnector/restapi/javaclient/builder/generate/codegen/support/CodegenFilenameSupport.java
@@ -2,7 +2,13 @@ package com.radiantlogic.dataconnector.restapi.javaclient.builder.generate.codeg
 
 /**
  * There is an issue where anonymous schemas will be automatically assigned names by
- * openapi-generator. Those names may clash with names that don't
+ * openapi-generator. Those names may clash with names of schemas that were explicitly declared in
+ * the spec. The generator doesn't catch these because they still end up differing by case, but many
+ * filesystems don't support case-sensitive names and so when things are written out one will
+ * overwrite the other and then the output is invalid.
+ *
+ * <p>This identifies and adds a suffix to names that will clash when being written out, so that
+ * they are safe and everything will be written correctly.
  */
 public class CodegenFilenameSupport {
   // TOOD finish this

d322c98f3516d0e60b1035e08c531dfa97cce356
Craig Miller <cmiller@radiantlogic.com>
2025-06-24 15:53:29 -0400

    Working on tests

--- rest-api-java-client-builder/src/test/java/com/radiantlogic/dataconnector/restapi/javaclient/builder/generate/codegen/support/CodegenFilenameSupportTest.java
+++ rest-api-java-client-builder/src/test/java/com/radiantlogic/dataconnector/restapi/javaclient/builder/generate/codegen/support/CodegenFilenameSupportTest.java
@@ -1,5 +1,6 @@
 package com.radiantlogic.dataconnector.restapi.javaclient.builder.generate.codegen.support;
 
+import lombok.NonNull;
 import org.junit.jupiter.api.Test;
 
 public class CodegenFilenameSupportTest {
@@ -11,5 +12,11 @@ public class CodegenFilenameSupportTest {
   @Test
   void itAddsSuffixToNameAndFixesImportsIfClash() {
     throw new RuntimeException();
+  }
+
+  @NonNull
+  private String modelFilename(
+      @NonNull final String templateName, @NonNull final String modelName) {
+    return modelName;
   }
 }

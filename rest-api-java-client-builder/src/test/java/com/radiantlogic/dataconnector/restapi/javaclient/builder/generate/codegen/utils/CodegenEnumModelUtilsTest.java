410ab2ba5dea673da1243a95c56522af2cc2874c
Craig Miller <cmiller@radiantlogic.com>
2025-06-25 16:18:54 -0400

    Stubbed out test

--- rest-api-java-client-builder/src/test/java/com/radiantlogic/dataconnector/restapi/javaclient/builder/generate/codegen/utils/CodegenEnumModelUtilsTest.java
+++ rest-api-java-client-builder/src/test/java/com/radiantlogic/dataconnector/restapi/javaclient/builder/generate/codegen/utils/CodegenEnumModelUtilsTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 public class CodegenEnumModelUtilsTest {
   @Test
-  void test() {
+  void itCreatesEnumModelFromEnumProp() {
     throw new RuntimeException();
   }
 }

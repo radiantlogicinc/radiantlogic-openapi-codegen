6f4eab81adf8ffc337b8a273e5daffeadde1e7e2
Craig Miller <cmiller@radiantlogic.com>
2025-06-26 09:56:38 -0400

    Progress

--- rest-api-java-client-builder/src/main/java/com/radiantlogic/dataconnector/restapi/javaclient/builder/generate/codegen/support/NewEnumProcessorSupport.java
+++ rest-api-java-client-builder/src/main/java/com/radiantlogic/dataconnector/restapi/javaclient/builder/generate/codegen/support/NewEnumProcessorSupport.java
@@ -1,5 +1,11 @@
 package com.radiantlogic.dataconnector.restapi.javaclient.builder.generate.codegen.support;
 
+import java.util.List;
+import java.util.Map;
+import lombok.NonNull;
+import org.openapitools.codegen.CodegenModel;
+import org.openapitools.codegen.model.ModelsMap;
+
 /**
  * Other support classes extract a lot of enums from models. These are inline enums that should
  * actually be separate models because inline enums produce more potential compile errors than any
@@ -8,4 +14,8 @@ package com.radiantlogic.dataconnector.restapi.javaclient.builder.generate.codeg
  * merges the new enums into the existing ModelMaps map so that the codegen can correctly generate
  * the necessary model classes.
  */
-public class NewEnumProcessorSupport {}
+public class NewEnumProcessorSupport {
+  public void processNewEnumsAndMergeToModelMaps(
+      @NonNull final List<CodegenModel> newEnums,
+      @NonNull final Map<String, ModelsMap> allModelMaps) {}
+}

15938fcd6dee50c22faaecba66bf7d611d7a5cc5
Craig Miller <cmiller@radiantlogic.com>
2025-06-26 16:34:23 -0400

    Progress

--- codegen-modules/openapi-java-client-codegen/src/main/java/com/radiantlogic/openapi/codegen/javaclient/generate/models/ExtendedCodegenDiscriminator.java
+++ codegen-modules/openapi-java-client-codegen/src/main/java/com/radiantlogic/openapi/codegen/javaclient/generate/models/ExtendedCodegenDiscriminator.java
@@ -5,7 +5,6 @@ import lombok.Getter;
 import lombok.Setter;
 import org.openapitools.codegen.CodegenDiscriminator;
 
-// TODO is not necessary, delete this
 /**
  * Extended version of the CodegenDiscriminator to add extra properties to deal with a lack of a
  * mapping in the spec.
@@ -15,7 +14,11 @@ import org.openapitools.codegen.CodegenDiscriminator;
 @EqualsAndHashCode(callSuper = true)
 public class ExtendedCodegenDiscriminator extends CodegenDiscriminator {
 
-  public boolean hasNoMapping() {
-    return getMapping() == null || getMapping().isEmpty();
+  public boolean useConstructor() {
+    return getPropertyType().equals("BigDecimal");
+  }
+
+  public boolean useCast() {
+    return !getIsEnum() && !useConstructor();
   }
 }

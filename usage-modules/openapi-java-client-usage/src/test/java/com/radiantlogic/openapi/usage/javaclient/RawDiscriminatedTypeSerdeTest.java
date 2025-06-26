1a22f91a5a67ae65fe578ffc8b850f408cedbb4e
Craig Miller <cmiller@radiantlogic.com>
2025-06-26 15:02:56 -0400

    stubbing new test

---
+++ usage-modules/openapi-java-client-usage/src/test/java/com/radiantlogic/openapi/usage/javaclient/RawDiscriminatedTypeSerdeTest.java
@@ -0,0 +1,14 @@
+package com.radiantlogic.openapi.usage.javaclient;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * This is a test of the de-serialization of the .Raw type for a discriminated union that lacks a
+ * discriminator.
+ */
+public class RawDiscriminatedTypeSerdeTest {
+  @Test
+  void itDeserializedRawDiscriminatedType() {
+    throw new RuntimeException();
+  }
+}

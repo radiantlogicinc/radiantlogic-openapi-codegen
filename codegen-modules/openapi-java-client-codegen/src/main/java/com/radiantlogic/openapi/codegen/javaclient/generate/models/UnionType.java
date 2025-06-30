f4cd872a995169c76dfe709886e09b0980245109
Craig Miller <cmiller@radiantlogic.com>
2025-06-30 09:08:51 -0400

    Updated enum

--- codegen-modules/openapi-java-client-codegen/src/main/java/com/radiantlogic/openapi/codegen/javaclient/generate/models/UnionType.java
+++ codegen-modules/openapi-java-client-codegen/src/main/java/com/radiantlogic/openapi/codegen/javaclient/generate/models/UnionType.java
@@ -1,25 +1,32 @@
 package com.radiantlogic.openapi.codegen.javaclient.generate.models;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
 /** Defines the type of union, if any, that the model represents. */
+@Getter
+@RequiredArgsConstructor
 public enum UnionType {
   /** The type is not a union type. This is valid. */
-  NO_UNION,
+  NO_UNION(true),
 
   /**
    * The type is a union type, but has no discriminator. This means a oneOf mapping and nothing
    * else. This is invalid.
    */
-  UNION_NO_DISCRIMINATOR,
+  UNION_NO_DISCRIMINATOR(false),
 
   /**
    * The type is a discriminated union type but has no mapping. This means a oneOf mapping, a
    * discriminator, but no discriminator mapping. This is invalid.
    */
-  DISCRIMINATED_UNION_NO_MAPPING,
+  DISCRIMINATED_UNION_NO_MAPPING(false),
 
   /**
    * The type is a discriminated union with its mapping. This means a oneOf mapping, a
    * discriminator, and a discriminator mapping. This is valid.
    */
-  DISCRIMINATED_UNION_WITH_MAPPING
+  DISCRIMINATED_UNION_WITH_MAPPING(true);
+
+  private final boolean isValid;
 }

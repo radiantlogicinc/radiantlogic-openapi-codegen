e0731fe857da76fe1589971570a8328f3f7bccd1
Craig Miller <cmiller@radiantlogic.com>
2025-06-30 15:24:01 -0400

    Progress with tests

--- codegen-modules/openapi-java-client-codegen/src/test/java/com/radiantlogic/openapi/codegen/javaclient/integration/DownloadSpecIT.java
+++ codegen-modules/openapi-java-client-codegen/src/test/java/com/radiantlogic/openapi/codegen/javaclient/integration/DownloadSpecIT.java
@@ -1,3 +1,16 @@
 package com.radiantlogic.openapi.codegen.javaclient.integration;
 
-public class DownloadSpecIT {}
+import com.github.tomakehurst.wiremock.junit5.WireMockTest;
+import org.junit.jupiter.api.Test;
+
+/**
+ * This test validates that an OpenAPI spec URL that is provided as an argument will be properly
+ * resolved and downloaded.
+ */
+@WireMockTest(httpPort = 9000)
+public class DownloadSpecIT {
+  @Test
+  void itDownloadsSpecUrl() {
+    throw new RuntimeException();
+  }
+}

82166ec7cc685333e5bc4edce047b80b33096bce
Craig Miller <cmiller@radiantlogic.com>
2025-06-30 15:14:36 -0400

    Fixing compile errors

--- codegen-modules/openapi-java-client-codegen/src/test/java/com/radiantlogic/openapi/codegen/javaclient/args/ArgsParserTest.java
+++ codegen-modules/openapi-java-client-codegen/src/test/java/com/radiantlogic/openapi/codegen/javaclient/args/ArgsParserTest.java
@@ -4,6 +4,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.radiantlogic.openapi.codegen.javaclient.properties.Props;
+import java.net.URI;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 
 public class ArgsParserTest {
@@ -11,29 +13,34 @@ public class ArgsParserTest {
   private final ArgsParser argsParser = new ArgsParser(props);
 
   @Test
+  @SneakyThrows
   void itShowsHelpMenu() {
     final String[] args = {"-h"};
     final Args parsedArgs = argsParser.parse(args);
-    final Args expectedArgs = new Args(ProgramArgStatus.EXIT, "", "");
+    final Args expectedArgs =
+        new Args(ProgramArgStatus.EXIT, new URI("http://localhost").toURL(), "");
     assertThat(parsedArgs).isEqualTo(expectedArgs);
   }
 
   @Test
+  @SneakyThrows
   void itParsesWithDefaults() {
     final String path = "http://localhost:8080/openapi.json";
     final String[] args = {"-p=%s".formatted(path)};
     final Args parsedArgs = argsParser.parse(args);
-    final Args expectedArgs = new Args(ProgramArgStatus.PROCEED, path, ArgsParser.DEFAULT_GROUP_ID);
+    final Args expectedArgs =
+        new Args(ProgramArgStatus.PROCEED, new URI(path).toURL(), ArgsParser.DEFAULT_GROUP_ID);
     assertThat(parsedArgs).isEqualTo(expectedArgs);
   }
 
   @Test
+  @SneakyThrows
   void itParsesWithAllArgsProvided() {
     final String path = "http://localhost:8080/openapi.json";
     final String groupId = "org.something";
     final String[] args = {"-p=%s".formatted(path), "-g=%s".formatted(groupId)};
     final Args parsedArgs = argsParser.parse(args);
-    final Args expectedArgs = new Args(ProgramArgStatus.PROCEED, path, groupId);
+    final Args expectedArgs = new Args(ProgramArgStatus.PROCEED, new URI(path).toURL(), groupId);
     assertThat(parsedArgs).isEqualTo(expectedArgs);
   }
 

84cfb065614f5948cf1aa0bf43077674ad717ce7
Craig Miller <cmiller@radiantlogic.com>
2025-07-02 10:45:42 -0400

    Working on test

--- usage-modules/openapi-java-client-usage/src/test/java/com/radiantlogic/openapi/usage/javaclient/FormUrlencodedTest.java
+++ usage-modules/openapi-java-client-usage/src/test/java/com/radiantlogic/openapi/usage/javaclient/FormUrlencodedTest.java
@@ -3,13 +3,19 @@ package com.radiantlogic.openapi.usage.javaclient;
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.post;
-import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
-import static com.github.tomakehurst.wiremock.client.WireMock.verify;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.tomakehurst.wiremock.junit5.WireMockTest;
+import com.radiantlogic.openapi.generated.oktaopenidconnectoauth20.api.OrgAsApi;
+import com.radiantlogic.openapi.generated.oktaopenidconnectoauth20.invoker.ApiClient;
+import com.radiantlogic.openapi.generated.oktaopenidconnectoauth20.model.AcrValue;
+import com.radiantlogic.openapi.generated.oktaopenidconnectoauth20.model.AmrValue;
+import com.radiantlogic.openapi.generated.oktaopenidconnectoauth20.model.CodeChallengeMethod;
+import com.radiantlogic.openapi.generated.oktaopenidconnectoauth20.model.Prompt;
+import com.radiantlogic.openapi.generated.oktaopenidconnectoauth20.model.ResponseMode;
+import com.radiantlogic.openapi.generated.oktaopenidconnectoauth20.model.ResponseTypesSupported;
 import java.util.HashMap;
 import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
@@ -18,21 +24,19 @@ import org.junit.jupiter.api.Test;
 @WireMockTest(httpPort = 9000)
 public class FormUrlencodedTest {
   private final ObjectMapper objectMapper = new ObjectMapper();
-
-  //  private AuthApi authApi;
+  private OrgAsApi orgAsApi;
 
   @BeforeEach
   void setUp() {
-    //    final ApiClient apiClient = new ApiClient();
-    //    apiClient.setBasePath(BASE_URL);
-    //    apiClient.setDebugging(true);
-    //    authApi = new AuthApi(apiClient);
+    final ApiClient apiClient = ApiClientSupport.createOktaOpenidConnectApiClient();
+    orgAsApi = new OrgAsApi(apiClient);
   }
 
   @Test
   void testFormUrlencodedRequest() throws Exception {
-    final String email = "testuser";
-    final String password = "password123";
+    final String clientId = "client-id";
+    final String codeChallenge = "challenge";
+    final CodeChallengeMethod codeChallengeMethod = CodeChallengeMethod.S256;
 
     final Map<String, Object> expectedResponse = new HashMap<>();
     expectedResponse.put("access_token", "test-token");
@@ -40,15 +44,38 @@ public class FormUrlencodedTest {
     expectedResponse.put("expires_in", 3600);
 
     stubFor(
-        post(urlPathEqualTo("/eoc-backend/auth/local"))
+        post(urlPathEqualTo("/oauth2/v1/authorize"))
             .withHeader("Content-Type", equalTo("application/x-www-form-urlencoded;charset=UTF-8"))
-            .withFormParam("email", equalTo(email))
-            .withFormParam("password", equalTo(password))
+            .withFormParam("client_id", equalTo(clientId))
+            .withFormParam("code_challenge", equalTo(codeChallenge))
+            .withFormParam("code_challenge_method", equalTo(codeChallengeMethod.getValue()))
             .willReturn(
                 aResponse()
                     .withStatus(201)
                     .withHeader("Content-Type", "application/json")
                     .withBody(objectMapper.writeValueAsString(expectedResponse))));
+
+    orgAsApi.authorize(
+        clientId,
+        "",
+        ResponseTypesSupported.CODE,
+        "",
+        "",
+        AcrValue.PHR,
+        codeChallenge,
+        codeChallengeMethod,
+        "",
+        AmrValue.DUO,
+        "",
+        "",
+        "",
+        10,
+        "",
+        Prompt.NONE,
+        ResponseMode.FORM_POST,
+        "",
+        "",
+        "");
 
     //    final Object result = authApi.authControllerLocalLogin(email, password, null);
     //
@@ -58,10 +85,11 @@ public class FormUrlencodedTest {
     //
     //    assertThat(resultMap).usingRecursiveComparison().isEqualTo(expectedResponse);
 
-    verify(
-        postRequestedFor(urlPathEqualTo("/eoc-backend/auth/local"))
-            .withHeader("Content-Type", equalTo("application/x-www-form-urlencoded;charset=UTF-8"))
-            .withFormParam("email", equalTo(email))
-            .withFormParam("password", equalTo(password)));
+    //    verify(
+    //        postRequestedFor(urlPathEqualTo("/eoc-backend/auth/local"))
+    //            .withHeader("Content-Type",
+    // equalTo("application/x-www-form-urlencoded;charset=UTF-8"))
+    //            .withFormParam("email", equalTo(email))
+    //            .withFormParam("password", equalTo(password)));
   }
 }

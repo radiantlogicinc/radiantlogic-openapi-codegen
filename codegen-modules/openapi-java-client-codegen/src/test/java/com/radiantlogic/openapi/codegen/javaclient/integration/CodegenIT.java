8c7dae4f8c89e2cffebd8e5b92816556435cf7d4
Craig Miller <cmiller@radiantlogic.com>
2025-07-02 09:31:10 -0400

    Another attempt

--- codegen-modules/openapi-java-client-codegen/src/test/java/com/radiantlogic/openapi/codegen/javaclient/integration/CodegenIT.java
+++ codegen-modules/openapi-java-client-codegen/src/test/java/com/radiantlogic/openapi/codegen/javaclient/integration/CodegenIT.java
@@ -215,6 +215,7 @@ public class CodegenIT {
 
     // TODO delete this block
     System.out.println("OUTPUT EXISTS: " + Files.exists(outputPath));
+    Files.list(OUTPUT_DIR).forEach(output -> System.out.println("OUTPUT: " + output));
     runProcess("echo $PATH", outputPath);
 
     System.out.printf("Codegen complete. Building generated code at %s%n", outputPath);
